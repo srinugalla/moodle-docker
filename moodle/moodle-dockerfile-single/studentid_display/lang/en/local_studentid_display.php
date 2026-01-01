8cc09c1c0135ec54f46cbcc49b9777624b9f047c
Shree <srinu.galla@gmail.com>
2026-01-01 00:06:40 +0000

    removed everything else except mdms

--- moodle/moodle-dockerfile-single/studentid_display/lang/en/local_studentid_display.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$string['pluginname'] = 'Student ID Display';
-$string['studentinfo'] = 'Student: {name} (ID: {id})';
-$string['noidnumber'] = 'Not set';
